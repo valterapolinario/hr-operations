dc8025a41839c73a57e91a5f9be53eb03e779160
valter.junior <valter_123@hotmail.com>
2024-05-17 10:24:19 -0300

    commit inicial do projeto

---
+++ src/main/kotlin/com/br/acc/hr/HrApplication.kt
@@ -0,0 +1,11 @@
+package com.br.acc.hr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrApplication>(*args)
+}
---
+++ src/test/kotlin/com/br/acc/hr/HrApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.acc.hr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HrApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
